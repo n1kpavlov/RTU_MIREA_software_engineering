ff3e06f04fd8ee595ae126cab77a8c23ac042a5d
Pavlov Nikita <nikniknik388@gmail.com>
2025-12-23 15:04:59 +0300

    Add files via upload

---
+++ 3_semester/Системы и алгоритмы обработки данных/Работа 5/СиАОД_5_1/СиАОД_5_1.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
+#include <bitset>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+
+    //Задание 1
+    cout << "Задание 1\n\n";
+
+    unsigned int x = 200, maska_1 = 1;
+    cout << "Исходное число: " << x << endl;
+    x &= (~(maska_1 << 4));//пятый разряд в 0
+    cout << "После преобразования: " << x << endl;
+    x = 11;
+    maska_1 = 1;
+    cout << "Исходное число: " << x << endl;
+    x |= (maska_1 << 6);//седьмой разряд в 1
+    cout << "После преобразования: " << x << endl << endl;
+
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    x = 25;
+    const int n = sizeof(int) * 8;
+    unsigned maska = (1 << n - 1);
+    cout << "Начальный вид маски: " << bitset<n>(maska) << endl;
+    cout << "Результат: ";
+    for (int i = 1; i <= n; i++) {
+        cout << ((x & maska) >> (n - i));
+        maska = maska >> 1;
+    }
+    cout << endl;
+
+
+    //Задание 2
+    cout << "\n\nЗадание 2\n\n";
+
+    unsigned char bitmas = 0;
+    cout << "Введите до 8 чисел (значения от 0 до 7, 8 - завершение ввода): ";
+    cin >> x;
+    while (x != 8) {
+        bitmas |= (1 << (7 - x));
+        cin >> x;
+    }
+    cout << "Отсортированны массив:";
+    bitset<8> sort = bitmas;
+    for (int i = 7; i >= 0; i--) {
+        if (sort[i] == 1) {
+            cout << " " << 7 - i;
+        }
+    }
+
+    unsigned long long mas = 0;
+    cout << "\nВведите до 64 чисел (значения от 0 до 63, 64 - завершение ввода): ";
+    cin >> x;
+    while (x != 64) {
+        mas |= (static_cast<unsigned long long>(1) << (63 - x));
+        cin >> x;
+    }
+    cout << "Отсортированны массив:";
+    bitset<64> srt = mas;
+    for (int i = 63; i >= 0; i--) {
+        if (srt[i] == 1) {
+            cout << " " << 63 - i;
+        }
+    }
+
+    unsigned char mass[8]{ 0 };
+    cout << "\nВведите до 64 чисел (значения от 0 до 63, 64 - завершение ввода): ";
+    cin >> x;
+    while (x != 64) {
+        mass[x / 8] |= (1 << (7 - x % 8));
+        cin >> x;
+    }
+    cout << "Отсортированны массив:";
+    for (int i = 0; i < 8; i++) {
+        bitset<8> sorting = mass[i];
+        for (int j = 7; j >= 0; j--) {
+            if (sorting[j] == 1) {
+                cout << " " << i * 8 + 7 - j;
+            }
+        }
+    }
+
+
+    //Задание 3
+    cout << "\n\nЗадание 3\n\n";
+
+    long int max = 8 * 1024 * 1024;
+    fstream fout("input.txt");
+    for (long int i = 1; i < max; i++) {
+        fout << max - i << " ";
+    }
+    fout << max;
+    fout.close();
+
+    unsigned int start = clock();
+    const long int bitset_size = 8 * 1024 * 1024;
+    bitset<bitset_size>* numbers = new bitset<bitset_size>;
+    fstream fin("input.txt");
+    while (fin >> x) {
+        numbers->set(x, 1);
+    }
+    fin.close();
+    fout.open("output.txt");
+    for (long int i = 0; i < bitset_size; i++) {
+        if (numbers->test(i)) {
+            fout << i << " ";
+        }
+    }
+    fout.close();
+    unsigned int end = clock();
+
+    cout << "Время работы алгоритма: " << end - start <<  " ms" << endl;
+    cout << "Размер массива: " << sizeof(*numbers) << " bytes" << endl;
+}
---
+++ 3_semester/Системы и алгоритмы обработки данных/Работа 5/СиАОД_5_2/СиАОД_5_2.cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+#include <bitset>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+struct book {
+    
+    long long isbn;
+    string author;
+    string name;
+
+    string out() {
+        return to_string(isbn) + ", " + author + ", " + name + "\n";
+    }
+};
+
+bool comp(const pair<book, int>& left, const pair<book, int>& right) {
+    return left.first.isbn < right.first.isbn;
+}
+
+int search_key(vector<pair<book, int>> tabl, vector<int> table, long long key) {
+    int i = table[0] - 1;
+    bool f = false;
+    if (key >= tabl[0].first.isbn && key <= tabl[tabl.size() - 1].first.isbn) {
+        for (int j = 1; j < table.size(); j++) {
+            if (tabl[i].first.isbn == key) {
+                f = true;
+                break;
+            }
+            else if (tabl[i].first.isbn < key) {
+                i += table[j];
+            }
+            else {
+                i -= table[j];
+            }
+        }
+        if (f) {
+            return tabl[i].second;
+        }
+        else {
+            return -1;
+        }
+    }
+    else {
+        return -1;
+    }
+    return 0;
+}
+
+string search_url(string s, int url) {
+    unsigned int start = clock();
+    ifstream fin(s, ios::binary | ios::in);
+    fin.seekg(sizeof(book) * (url - 1), ios_base::beg);
+    book test;
+    fin.read((char*)&test, sizeof(book));
+    fin.close();
+    unsigned int end = clock();
+    cout << "Время работы программы: " << end - start << " ms" << endl;
+    return test.out();
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(0));
+    vector<pair<book, int>> tabl;
+
+    int x, size = rand() % 100;
+    book test;
+
+    ofstream fout("books.bin", ios::binary | ios::out);
+    for (int i = 0; i < size; i++) {
+        test.isbn = 0;
+        for (int j = 0; j < 12; j++) {
+            x = rand() % 10;
+            if (x == 0 && j == 11) {
+                test.isbn += (x + 1) * pow(10, j);
+            }
+            else {
+                test.isbn += x * pow(10, j);
+            }
+        }
+        test.author = "Vasya_" + to_string(size - i);
+        test.name = "book_" + to_string(i);
+        fout.write((char*)&test, sizeof(book));
+    }
+    fout.close();
+
+
+    //Линейный поиск
+    
+    unsigned int start = clock();
+    ifstream fin("books.bin", ios::binary | ios::in);
+    long long key;
+    int number = 0;
+    bool f = false;
+    cout << "Введите ключ для поиска: ";
+    cin >> key;
+    while (fin.read((char*)&test, sizeof(book))) {
+        number++;
+        if (test.isbn == key) {
+            f = true;
+            break;
+        }
+    }
+    if (f) {
+        cout << "Искомый ключ в " << number << " строке" << endl;
+    }
+    else {
+        cout << "Ключ не найден" << endl;
+    }
+    fin.close();
+    unsigned int end = clock();
+    cout << "Время работы программы: " << end - start << " ms"  << endl;
+
+
+    //однородный бинарный поиск с таблицей смещений
+
+    fin.open("books.bin", ios::binary | ios::in);
+    for (int i = 0; i < size; i++) {
+        fin.read((char*)&test, sizeof(book));
+        tabl.push_back(make_pair(test, i + 1));
+    }
+    fin.close();
+
+    sort(tabl.begin(), tabl.end(), comp);
+
+    for (auto element : tabl) {
+        cout << element.second << " " << element.first.out();
+    }
+
+    vector <int> table;
+    for (int j = 1; j <= log2(tabl.size()) + 2; j++) {
+        table.push_back((tabl.size() + pow(2, j - 1)) / pow(2, j));
+    }
+
+    cout << "Введите ключ для поиска: ";
+    cin >> key;
+    start = clock();
+    int out = search_key(tabl, table, key);
+    end = clock();
+    cout << "Время работы программы: " << end - start << " ms" << endl;
+    if (out >= 0) {
+        cout << "Искомый ключ в " << out << " строке" << endl;
+    }
+    else {
+        cout << "Ключ не найден" << endl;
+    }
+
+
+    //прямой доступ к записи файла
+
+    int url;
+    cout << "Введите номер записи: ";
+    cin >> url;
+    string s = "books.bin";
+    cout << "Найдена запись: " << search_url(s, url);
+}
